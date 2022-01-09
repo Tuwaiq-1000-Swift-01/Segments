a8f0d61052bfdc48cebe93c14a0a5b016f63ff15
Aisha Ali <aamiss1496@gmail.com>
2022-01-09 21:02:04 +0300

    Segment Control Update

--- segmentControl-Task1-Aisha/segmentControl/ViewController.swift
+++ segmentControl-Task1-Aisha/segmentControl/ViewController.swift
@@ -14,41 +14,46 @@ class ViewController: UIViewController {
   let array3 = ["Amerah", "Aisha", "Hajer", "Areej"]
   let array4 = ["Aisha", "Remaa", "Reem", "Fouz", "Sara"]
   let array5 = ["Aisha", "Remaa", "Reem", "Fouz", "Sara", "Aisha"]
-  let segmentedControl2 = UISegmentedControl()
+  
+  
+  let segmentedControl2 : UISegmentedControl = {
+    let segmentedControl2 = UISegmentedControl()
+    segmentedControl2.translatesAutoresizingMaskIntoConstraints = false
+    return segmentedControl2
+  } ()
+  
+  
+  let segmentedControl : UISegmentedControl = {
+    let segmentedControl = UISegmentedControl(items: ["2","3","4","5"])
+    segmentedControl.selectedSegmentIndex = 0
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    return segmentedControl
+  } ()
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    
   }
   
-  override func viewWillAppear(_ animated: Bool) {
+  
+  override func loadView() {
+    super.loadView()
     
-    let segmentedControl = UISegmentedControl(items: ["2","3","4","5"])
-    segmentedControl.backgroundColor = UIColor.systemBackground
-    segmentedControl.selectedSegmentIndex = 0
-    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    
+    
     view.addSubview(segmentedControl)
-    
-    
-
-    
     segmentedControl.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     segmentedControl.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     
     
     
-    segmentedControl2.backgroundColor = UIColor.systemBackground
-    segmentedControl2.translatesAutoresizingMaskIntoConstraints = false
     
     view.addSubview(segmentedControl2)
-    
     segmentedControl2.topAnchor.constraint(equalTo: segmentedControl.topAnchor, constant: 50).isActive = true
-
     segmentedControl2.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     segmentedControl2.updateTitle(array: array2)
 
+    //
     segmentedControl.addTarget(self, action: #selector(changeSegment), for: .valueChanged)
     
   }
@@ -59,37 +64,23 @@ class ViewController: UIViewController {
     
     switch seg.selectedSegmentIndex {
     case 0 :
-      
- 
       segmentedControl2.updateTitle(array: array2)
-      //      segmentedControl2.upda
       
       
     case 1:
-  
       segmentedControl2.updateTitle(array: array3)
 
-      
-      
-      
     case 2:
       segmentedControl2.updateTitle(array: array4)
 
-      
-      
+
     case 3:
       segmentedControl2.updateTitle(array: array5)
 
-   
-      
     default:
       break
       
     }
-    
-    
-    
-    
     
   }
 }
@@ -101,7 +92,7 @@ extension UISegmentedControl {
     for t in titles {
       insertSegment(withTitle: t, at: numberOfSegments, animated: true)
     }
-    
+    selectedSegmentIndex = 0
   }
 }
 
